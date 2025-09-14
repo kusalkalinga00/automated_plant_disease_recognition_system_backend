0539d27ee2c4278080736f413535a1ce54c423e7
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-09-14 12:21:54 +0530

    Add admin routes for managing diseases and treatments; implement CRUD operations

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from .routes_health import router as health_router
 from .auth import router as auth_router
 from .routes_db import router as db_router
 from .scans import router as scans_router
+from .admin import router as admin_router
 
 
 api_v1 = APIRouter()
@@ -10,3 +11,4 @@ api_v1.include_router(health_router, prefix="")
 api_v1.include_router(auth_router, prefix="")
 api_v1.include_router(db_router, prefix="")
 api_v1.include_router(scans_router, prefix="")
+api_v1.include_router(admin_router, prefix="")
